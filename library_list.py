e51896f69c4c7334169cfe35a5b10d213ef883f1
JonShibata <jon.shibata@outlook.com>
2022-08-06 01:10:31 -0400

    Use selenium to pull html automatically

--- library_list.py
+++ library_list.py
@@ -5,29 +5,31 @@ import os
 import subprocess
 import re
 
-from tkinter.filedialog import askopenfilenames
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 
-def print_library_list():
 
-    html_files = askopenfilenames(title="Select Library Lists",
-                                 filetypes=[(".HTML", ".html")])
+def print_library_list(html_str):
+
 
     book_list_all = set()
 
-    for html_file in html_files:
+    book_list = re.findall("{}".format(
+        'Title: .*?<.*?-c521="">(.*?)<.*?DateDue:.*?<.*?-c521="">([0-9]{1,2}/[0-9]{1,2})/'),
+        html_str, flags=re.DOTALL)
 
-        with open(html_file) as html_str:
-            book_list = re.findall("{}".format(
-                'Title: .*?<.*?-c521="">(.*?)<'),
-                html_str.read(), flags=re.DOTALL)
-
-        for book_title in book_list:
-            book_list_all.add(book_title)
+    for book_info in book_list:
+        title_str = book_info[0].replace("&amp;", "&")
+        book_list_all.add("{}-{}".format(book_info[1], title_str))
 
 
     book_list_all = sorted(book_list_all)
-
+    
     outfile = re.sub(os.path.basename(__file__), "output.txt", os.path.abspath(__file__))
 
     with open(outfile, "w") as outfile:
@@ -35,6 +37,52 @@ def print_library_list():
             print(book_title)
             outfile.write("{}\n".format(book_title))
 
+    outfile = re.sub(os.path.basename(__file__), "raw_html.txt", os.path.abspath(__file__))
+
+    with open(outfile, "w") as outfile:
+        outfile.write(html_str)
+
+
     # subprocess.run("gedit {}".format(outfile))
 
-print_library_list()
+
+
+
+
+def get_library_data():
+
+    id_list = ("903675", "902232", "6596")
+
+
+    browser = webdriver.Chrome()
+    browser.set_window_size(1800, 1200)
+    html_str = ""
+
+    for id in id_list:
+
+        browser.get('https://hamb.agverso.com/login?cid=hamb&lid=HAMB')
+
+        WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.ID, "username"))).send_keys(id)
+        
+        password_box = browser.find_element(By.ID, "password")
+        password_box.send_keys("library")
+        password_box.send_keys(Keys.RETURN) 
+
+        WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "fa-angle-down"))).click()
+
+        WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.ID, "itemsOut"))).click()
+
+        WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "mx-auto")))
+
+        html_str = html_str + browser.page_source
+
+        WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "fa-angle-down"))).click()
+
+        WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.ID, "logOut"))).click()
+
+    return (html_str)
+
+
+
+print_library_list(get_library_data())
+
